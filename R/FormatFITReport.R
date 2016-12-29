5fd7905f7ec05bc2f6b33bc2a9d0ca9d228f65da
Ryan Kuhn <kuhnrl30@gmail.com>
2016-12-29 10:04:48 -0500

    Basic package with FormatFITReport function

---
+++ R/FormatFITReport.R
@@ -0,0 +1,51 @@
+#' Process raw FIT data for computation
+#' 
+#' Preprocess an extract from the FIT financial reporting system and prepare
+#' it for further analysis.  The basic premise is to clean the column headers
+#' and remove any extraneous leading characters.
+#' 
+#' The data extract is created by using transaction code \code{
+#' s_pl0_86000030}.  In the search paramenter input screen, use the following 
+#' settings:
+#' 
+#' \itemize{
+#' \item{Company Code: }{1001, 1002}
+#' \item{Function: }{2001}
+#' \item{Ledger: }{0L}
+#' \item{Currency Type: }{10}
+#' }
+#' 
+#' @param file path to .csv file extracted from FIT.
+#' 
+#' @export
+#' @import dplyr
+
+FormatFITReport<- function(file){
+
+  dat<- readxl::read_excel(file)
+  
+  # The column headers span 2 rows in the excel extract so must also be
+  # picked up from the column names and the first row of the dataframe.
+  heads<- c(unlist(dat[1,1:10]),colnames(dat[,11:18]))
+  dat2<- as.data.frame(dat[-1,])
+  
+  # Clean up headers and save as column names
+  heads[14:15]<- c("debits","credits")
+  heads<- gsub("[[:space:]]", "", heads)
+  names(dat2)<- heads
+  
+  dat2[,11:18]<- lapply(dat2[,11:18], as.numeric)
+  
+  dat3<- dat2 %>%
+    mutate(Month= debits - credits) %>%
+    filter(Month != 0) %>%
+    mutate(AccountNumber= substr(AccountNumber, 6, 13),
+           ProfitCenter= substr(ProfitCenter, 6,13),
+           CompanyCode= substr(CompanyCode, 1,4),
+           FunctionalArea= substr(FunctionalArea, 1,4),
+           CostCenterNo= gsub("[[:alpha:]]|[[:punct:]]|[[:blank:]]", "",CostCenter),
+           CostCenter= trimws(gsub("OCDG/","",CostCenter))) %>%
+    select(CompanyCode, FunctionalArea, AccountNumber,   
+           ProfitCenter, CostCenter, CostCenterNo, Month)
+  dat3
+}
---
+++ R/Ortho.R
@@ -0,0 +1,17 @@
+#' Ortho
+#'
+#' Providing common functions for working with financial data
+#' at Ortho Clinical Diagnostics. The financial data is often 
+#' exported from FIT- an Oracle general ledger system. The
+#' extracts often do not have formatting conducive to analysis
+#' in R so require additional munging. This package includes 
+#' the convenience functions to perform this common task.
+#'
+#' @docType package
+#' @name Ortho
+#' @author Ryan Kuhn
+#' @importFrom readxl "read_excel"
+#' @import dplyr
+#' @import httr
+#'
+NULL
